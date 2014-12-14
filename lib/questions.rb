d50ff561455461adb7bcabe4e9173c954be78f5c
ed <ejbyne@gmail.com>
2014-12-14 12:34:29 +0000

    21 tests passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -117,14 +117,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
-  array.each { |number| return array.take(array.index(number)) if number > 5 }
+  array.each_with_index { |number, index| return array.take(index) if number > 5 }
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-  # array.to_h
+  array.each_slice(2).to_a.to_h
 end
 
 # get all the letters used in an array of words and return
