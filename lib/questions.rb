95d37af2d7b6a5ca9865898a9501e559178a8849
ed <ejbyne@gmail.com>
2014-12-15 17:52:04 +0000

    All 39 tests passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -278,6 +278,9 @@ end
 # and 1 that is 4 letters long. Return it as a hash in the format
 # word_length => count, e.g. {2 => 1, 3 => 5, 4 => 1}
 def count_words_of_each_length_in_a_file(file_path)
+  words = Hash.new(0)
+  File.open(file_path, 'r').read.scan(/\w+/).each { |word| words[word.length] += 1 }
+  words
 end
 
 # implement fizzbuzz without modulo, i.e. the % method
